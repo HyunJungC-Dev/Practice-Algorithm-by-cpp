e4a092821ef9374f4940ee67e676f0696ab32a19
HyunJung <HyunJungC.Dev@gmail.com>
2021-01-15 00:22:18 +0900

    sucess all test case!

--- Exercise-algorithm/ex1.cpp
+++ Exercise-algorithm/ex1.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
-
+// 1. 1부터 N까지 M의 배수합
 int main() {
-
+	int N, M, result = 0;
+	cin >> N >> M;
+	
+	int tmp = M;
+	while (tmp <= N) {
+		result += tmp;
+		tmp += M;
+	}
+	
+	cout << result;
 
 	return 0;
 }
